4cf46dea19d598b12524ed61d45b47f0fb348fdb
dborgards <dborgards@users.noreply.github.com>
2019-11-20 16:26:00 +0100

    Migration to csproj-Format 2019; Added Target .netstandard2.0 / .netcoreapp3.0

---
+++ DataMatrix.net/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1712:Do not prefix enum values with type name", Justification = "<Ausstehend>", Scope = "type", Target = "~T:DataMatrix.net.DmtxScheme")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1712:Do not prefix enum values with type name", Justification = "<Ausstehend>", Scope = "type", Target = "~T:DataMatrix.net.DmtxRange")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1712:Do not prefix enum values with type name", Justification = "<Ausstehend>", Scope = "type", Target = "~T:DataMatrix.net.DmtxMaskBit")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1712:Do not prefix enum values with type name", Justification = "<Ausstehend>", Scope = "type", Target = "~T:DataMatrix.net.DmtxEdge")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1712:Do not prefix enum values with type name", Justification = "<Ausstehend>", Scope = "type", Target = "~T:DataMatrix.net.DmtxFlip")]
